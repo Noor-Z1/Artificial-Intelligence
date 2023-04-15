d61c5325d8bcaeb4e9eac8d2a1e875c1da3728b5
Hevra Petekkaya <hevrapetekkaya1@gmail.com>
2023-04-15 18:54:24 +0300

    completed the printing on the screen to display the expansion

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ treasure_map = {"starting_point": [("stormy_ocean", 4), ("forest", 7)],
                 "desert": [("treasure", 10)],
                 "treasure": []}
 
-# Request 2. Apply Uniform cost search and A* to find the cheapest path to the treasure.
+# Request 2a. Apply Uniform cost search find the cheapest path to the treasure.
 def uniform_cost_search(graph, start, goal):
+    print("Applying UCS:")
+
     # Priority queue of (priority, node, path).
     # Initially, it only includes the start state <=> starting point.
     frontier = [(0, start, [])]
     # explored will keep track of the explored nodes
     explored = set()
+
+    print("Starting point: Cost is 0")
 
     # The while loop continues until the frontier queue becomes empty
     # (i.e., there are no more nodes to explore). Or, it is also possible
@@ -26,6 +30,7 @@ def uniform_cost_search(graph, start, goal):
 
         # check if we got to the goal. if so, end the loop and return the path to the goal
         if current == goal:
+            print("Solution Path:  ", "-> ".join(str(state) for state in path+[current]), " with total cost", priority)
             return path + [current]
 
         # if it is already explored, it means that we already added its
@@ -39,8 +44,14 @@ def uniform_cost_search(graph, start, goal):
         # check the treasure_map to find the neighbors and their respective costs
         for neighbor, cost in graph[current]:
             if neighbor not in explored:
+                print(current, "-", neighbor, ": Cost is", priority+cost)
                 heapq.heappush(frontier, (priority + cost, neighbor, path + [current]))
 
     # note the Completeness property: if there is a solution, the algorithm finds it, otherwise
     # it reports failure, which is returning none.
     return None
+
+path = uniform_cost_search(treasure_map, "starting_point", "treasure")
+
+# Request 2a. Apply A* to find the cheapest path to the treasure.
+
